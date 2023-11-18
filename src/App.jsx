154b72335699c4d7781e5169d2e775b333821bc3
Mobinshahidi <mowbinsh@gmail.com>
2023-11-18 18:57:46 +0330

    add styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,50 @@ import { useState } from 'react';
 
 function App() {
 	const [speed, setSpeed] = useState(1);
+
 	const speedHandler = () => {
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			const activeTabId = tabs[0].id;
-			chrome.tabs.sendMessage(activeTabId, { action: 'changePlaybackSpeed', speed });
-		  });
+		try {
+			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+				const activeTabId = tabs[0].id;
+				chrome.tabs.sendMessage(activeTabId, {
+					action: 'changePlaybackSpeed',
+					speed,
+				});
+			});
+		} catch (error) {
+			// Handle the error appropriately
+			console.error('Error in speedHandler:', error);
+		}
 	};
 	return (
 		<>
-			<h1>Video Speed Control</h1>
-			<p>Current speed : {speed}</p>
-			<input
-				type="number"
-				step="0.1"
-				value={speed}
-				onChange={(e) => {
-					setSpeed(parseFloat(e.target.value));
-				}}
-			/>
-			<button onClick={speedHandler}>Change Speed</button>
+			<div className="w-64 h-64">
+				<h1 className="items-center flex text-lg font-bold justify-center m-5">
+					Video Speed Control
+				</h1>
+				<p className="flex justify-center m-4 text-sm font-bold">
+					Speed : {speed}
+				</p>
+				<div className="flex flex-col justify-between items-center m-7">
+					<input
+						className="border-2 rounded"
+						type="number"
+						step="0.1"
+						value={speed}
+						onChange={(e) => {
+							e.target.value ? setSpeed(parseFloat(e.target.value)) : null;
+						}}
+					/>
+					{speed !== 0 && (
+						<button
+							className="m-6 p-2 border-2 rounded border-lime-950 bg-lime-950 text-white hover:bg-lime-900 hover:border-lime-900"
+							onClick={speedHandler}
+						>
+							Change Speed
+						</button>
+					)}
+				</div>
+			</div>
 		</>
 	);
 }
