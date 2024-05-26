b2f59b262d30c6c734664496d1b1e8d2bc6e3b8e
Mobinshahidi <157294761+mobinshahiidi@users.noreply.github.com>
2024-05-26 23:46:18 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 function App() {
-	const [speed, setSpeed] = useState(1);
-	const [hasVideo, setHasVideo] = useState(false);
+  const [speed, setSpeed] = useState(1);
+  const [hasVideo, setHasVideo] = useState(false);
 
-	useEffect(() => {
-		chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-			const activeTabId = tabs[0].id;
-			chrome.tabs.sendMessage(
-				activeTabId,
-				{ action: 'checkVideoPresence' },
-				(response) => {
-					setHasVideo(response.hasVideo);
-				},
-			);
-		});
-	}, []);
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const activeTabId = tabs[0].id;
+      chrome.tabs.sendMessage(
+        activeTabId,
+        { action: "checkVideoPresence" },
+        (response) => {
+          setHasVideo(response.hasVideo);
+        }
+      );
+    });
+  }, []);
 
-	const speedHandler = () => {
-		try {
-			chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-				const activeTabId = tabs[0].id;
-				chrome.tabs.sendMessage(activeTabId, {
-					action: 'changePlaybackSpeed',
-					speed,
-				});
-			});
-		} catch (error) {
-			console.error('Error in speedHandler:', error);
-		}
-	};
+  const speedHandler = () => {
+    try {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        const activeTabId = tabs[0].id;
+        chrome.tabs.sendMessage(activeTabId, {
+          action: "changePlaybackSpeed",
+          speed,
+        });
+      });
+    } catch (error) {
+      console.error("Error in speedHandler:", error);
+    }
+  };
 
-	return (
-		<>
-			<div className="w-64 h-64">
-				<h1 className="items-center flex text-lg font-bold justify-center m-5">
-					Video Speed Control
-				</h1>
-				{hasVideo ? (
-					<div>
-						<p className="flex justify-center m-4 text-sm font-bold">
-							Speed : {speed}
-						</p>
-						<div className="flex flex-col justify-between items-center m-7">
-							<input
-								className="border-2 rounded"
-								type="number"
-								step="0.1"
-								value={speed}
-								onChange={(e) => {
-									e.target.value ? setSpeed(parseFloat(e.target.value)) : null;
-								}}
-							/>
-							{speed !== 0 && (
-								<button
-									className="m-6 p-2 border-2 rounded border-lime-950 bg-lime-950 text-white hover:bg-lime-900 hover:border-lime-900"
-									onClick={speedHandler}
-								>
-									Change Speed
-								</button>
-							)}
-						</div>
-					</div>
-				) : (
-					<p className='flex justify-center my-20 text-xl font-bold'>It seems no video is here.</p>
-				)}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="w-64 h-64 font-mono font-bold">
+        <h1 className="flex text-[#183A37] text-xl font-bold justify-start m-5">
+          Rushify
+        </h1>
+        {hasVideo ? (
+          <div>
+            <div className="flex flex-row justify-center items-center m-7 font-extrabold">
+              <input
+                className="border-2 rounded mr-2 w-24 h-[38px]"
+                type="number"
+                step="0.1"
+                value={speed}
+                onChange={(e) => {
+                  e.target.value ? setSpeed(parseFloat(e.target.value)) : null;
+                }}
+              />
+              {speed !== 0 && (
+                <button
+                  className="my-6 p-2 ml-2 border-2 w-24 h-[38px] rounded border-[#183A37] bg-[#183A37] text-white hover:bg-[#04151F] hover:border-[#04151F]"
+                  onClick={speedHandler}
+                >
+                  Change!
+                </button>
+              )}
+            </div>
+            <p className="flex justify-center m-4 text-sm font-bold">
+              Speed : {speed}
+            </p>
+          </div>
+        ) : (
+          <div className="flex justify-center items-center my-20 mx-6 text-lg">
+            <p className="">
+              It seems no video is here!
+            </p>
+          </div>
+        )}
+        <div className="flex justify-center items-center text-md text-[#183A37] mt-6">
+          <a href="https://mobinshahidi.netlify.app">mobin shahidi</a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
